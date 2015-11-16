9b38a26930846967a21eebe668b25b78e4054c44
Asaf Peleg <asaf@locusenergy.com>
2015-11-16 16:03:58 -0500

    fixing indentation in setup.py

--- setup.py
+++ setup.py
@@ -3,36 +3,36 @@ from setuptools import setup
 from setuptools.command.test import test as TestCommand
 
 class PyTest(TestCommand):
-	user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
-	def initialize_options(self):
-		TestCommand.initialize_options(self)
-		self.pytest_args = []
+    user_options = [('pytest-args=', 'a', "Arguments to pass to py.test")]
+    def initialize_options(self):
+        TestCommand.initialize_options(self)
+        self.pytest_args = []
 
-	def finalize_options(self):
-		TestCommand.finalize_options(self)
-		self.test_args = []
-		self.test_suite = True
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = []
+        self.test_suite = True
 
-	def run_tests(self):
-		#import here, cause outside the eggs aren't loaded
-		import pytest
-		errno = pytest.main(self.pytest_args)
-		sys.exit(errno)
+    def run_tests(self):
+        # import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.pytest_args)
+        sys.exit(errno)
 
 if (sys.version_info[0], sys.version_info[1]) != (2,7):
-	print "WARNING: USING UNTESTED PYTHON VERSION."
+    print "WARNING: USING UNTESTED PYTHON VERSION."
 
 setup(
-	name='dbio',
-	version='0.4.5',
-	author='Locus Energy',
-	author_email='dbio@locusenergy.com',
-	license='MIT',
-	url='https://github.com/locusenergy/dbio',
-	download_url='https://github.com/LocusEnergy/dbio/tarball/0.4.4',
-	description='Simple module for database I/O operations.',
-	long_description=open("README.rst").read(),
-	classifiers=[
+    name='dbio',
+    version='0.4.5',
+    author='Locus Energy',
+    author_email='dbio@locusenergy.com',
+    license='MIT',
+    url='https://github.com/locusenergy/dbio',
+    download_url='https://github.com/LocusEnergy/dbio/tarball/0.4.4',
+    description='Simple module for database I/O operations.',
+    long_description=open("README.rst").read(),
+    classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: MIT License',
@@ -40,29 +40,29 @@ setup(
         'Topic :: Database',
     ],
 
-	packages=[
-		'dbio',
-		'dbio.databases'
-	],
-	entry_points={
-		'console_scripts': [
-			'dbio = dbio.__main__:main'
-		]
-	},
-	install_requires=[
-		'sqlalchemy',
-		'unicodecsv'
-	],
-	extras_require={
-		'MySQL' : ['MySQL-python'],
-		'Vertica' : ['vertica-python', 'sqlalchemy-vertica-python'],
-		'VerticaODBC' : ['pyodbc', 'vertica-sqlalchemy'],
-		'PostgreSQL' : ['psycopg2'],
-	},
-	tests_require=[
-		'pytest',
-	],
-	cmdclass= { 
-		'test' : PyTest,
-	},
+    packages=[
+        'dbio',
+        'dbio.databases'
+    ],
+    entry_points={
+        'console_scripts': [
+            'dbio = dbio.__main__:main'
+        ]
+    },
+    install_requires=[
+        'sqlalchemy',
+        'unicodecsv'
+    ],
+    extras_require={
+        'MySQL' : ['MySQL-python'],
+        'Vertica' : ['vertica-python', 'sqlalchemy-vertica-python'],
+        'VerticaODBC' : ['pyodbc', 'vertica-sqlalchemy'],
+        'PostgreSQL' : ['psycopg2'],
+    },
+    tests_require=[
+        'pytest',
+    ],
+    cmdclass= {
+        'test' : PyTest,
+    },
 )
